eddd8406097750b5178d809d466c08826bc2ac97
ranwei <rw1031818672@163.com>
2017-05-16 17:09:30 +0800

    解决结束日期当天的数据查不到的情况

---
+++ src/main/java/com/trs/gov/kpi/utils/InitEndTime.java
@@ -0,0 +1,28 @@
+package com.trs.gov.kpi.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by ranwei on 2017/5/16.
+ */
+public class InitEndTime {
+
+    public static String initTime(String endTime) {
+        String time = null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = sdf.parse(endTime);
+            Calendar calender = Calendar.getInstance();
+            calender.setTime(date);
+            calender.add(Calendar.DAY_OF_MONTH, 1);
+            time = sdf.format(calender.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return time;
+    }
+}
