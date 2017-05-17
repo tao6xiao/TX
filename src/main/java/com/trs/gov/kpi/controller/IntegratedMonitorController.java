63dd7def5b5b77318e18be40e2f2104b1cf369be
ranwei <rw1031818672@163.com>
2017-05-17 11:15:34 +0800

    新增 查询所有问题总数接口

--- src/main/java/com/trs/gov/kpi/controller/IntegratedMonitorController.java
+++ src/main/java/com/trs/gov/kpi/controller/IntegratedMonitorController.java
@@ -1,9 +1,12 @@
 package com.trs.gov.kpi.controller;
 
+import com.trs.gov.kpi.entity.IssueBase;
 import com.trs.gov.kpi.service.InfoErrorService;
 import com.trs.gov.kpi.service.InfoUpdateService;
 import com.trs.gov.kpi.service.LinkAvailabilityService;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -23,6 +26,14 @@ public class IntegratedMonitorController {
 
     @Resource
     private InfoErrorService infoErrorService;
+
+    @RequestMapping(value = "/all/count", method = RequestMethod.GET)
+    public Integer getAllIssueCount(@ModelAttribute IssueBase issueBase) {
+        int linkAvailabilityCount = linkAvailabilityService.getHandledIssueCount(issueBase) + linkAvailabilityService.getUnhandledIssueCount(issueBase);
+        int infoUpdateCount = infoUpdateService.getHandledIssueCount(issueBase) + infoUpdateService.getUpdateNotIntimeCount(issueBase) + infoUpdateService.getUpdateWarningCount(issueBase);
+        int infoErrorCount = infoErrorService.getHandledIssueCount(issueBase) + infoErrorService.getUnhandledIssueCount(issueBase);
+        return linkAvailabilityCount+infoErrorCount+infoUpdateCount;
+    }
 
 
 }
