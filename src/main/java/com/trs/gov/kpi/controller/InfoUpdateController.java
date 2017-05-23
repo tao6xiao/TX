a38c0628a7db13815804de00885e7e0af3a3cfe6
ranwei <rw1031818672@163.com>
2017-05-23 11:51:11 +0800

    增加分页参数校验

--- src/main/java/com/trs/gov/kpi/controller/InfoUpdateController.java
+++ src/main/java/com/trs/gov/kpi/controller/InfoUpdateController.java
@@ -7,10 +7,7 @@ import com.trs.gov.kpi.entity.exception.RemoteException;
 import com.trs.gov.kpi.entity.responsedata.ApiPageData;
 import com.trs.gov.kpi.entity.responsedata.Statistics;
 import com.trs.gov.kpi.service.InfoUpdateService;
-import com.trs.gov.kpi.utils.InitQueryFiled;
-import com.trs.gov.kpi.utils.IssueCounter;
-import com.trs.gov.kpi.utils.PageInfoDeal;
-import com.trs.gov.kpi.utils.ParamCheckUtil;
+import com.trs.gov.kpi.utils.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -67,8 +64,9 @@ public class InfoUpdateController {
      * @throws BizException
      */
     @RequestMapping(value = "/unhandled", method = RequestMethod.GET)
-    public ApiPageData getIssueList(Integer pageSize, Integer pageIndex, @ModelAttribute IssueBase issueBase) throws BizException {
+    public ApiPageData getIssueList(Integer pageIndex, Integer pageSize, @ModelAttribute IssueBase issueBase) throws BizException {
 
+        PagerCheckUtil.check(pageIndex, pageSize);
         if (issueBase.getSearchText() != null && !issueBase.getSearchText().trim().isEmpty()) {
             List list = InitQueryFiled.init(issueBase.getSearchText(), infoUpdateService);
             issueBase.setIds(list);
