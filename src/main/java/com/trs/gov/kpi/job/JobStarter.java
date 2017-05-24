79a95581ecc7e83403e8b1052037a121499343f7
linwei <lin.wei@trs.com.cn>
2017-05-24 14:22:19 +0800

    启动时，对所有设置的站点执行栏目更新检测

--- src/main/java/com/trs/gov/kpi/job/JobStarter.java
+++ src/main/java/com/trs/gov/kpi/job/JobStarter.java
@@ -1,6 +1,8 @@
 package com.trs.gov.kpi.job;
 
+import com.trs.gov.kpi.entity.MonitorSite;
 import com.trs.gov.kpi.scheduler.InfoUpdateCheckScheduler;
+import com.trs.gov.kpi.service.MonitorSiteService;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
@@ -13,6 +15,8 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+
+import java.util.List;
 
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
@@ -28,6 +32,9 @@ public class JobStarter implements ApplicationListener<ContextRefreshedEvent> {
     @Resource
     ApplicationContext applicationContext;
 
+    @Resource
+    MonitorSiteService monitorSiteService;
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 
@@ -38,28 +45,53 @@ public class JobStarter implements ApplicationListener<ContextRefreshedEvent> {
                 Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
                 scheduler.start();
 
-                JobDetail job = newJob(CheckJob.class)
-                        .withIdentity("infoUpdateCheckJob", "group-check")
-                        .build();
+                // 栏目更新检查
+                initInfoUpdateCheckJob(scheduler);
 
-                // 真正的执行任务
-                InfoUpdateCheckScheduler checkScheduler = applicationContext.getBean
-                        (InfoUpdateCheckScheduler.class);
-                checkScheduler.setSiteId(11);
-                checkScheduler.setBaseUrl("www.baidu.com");
-                job.getJobDataMap().put("task", checkScheduler);
-
-                Trigger trigger = newTrigger()
-                        .withIdentity("infoUpdateCheckJobTrigger", "group-check")
-                        .startNow()
-                        .withSchedule(simpleSchedule()
-                                .withIntervalInSeconds(24 * 60 * 60)
-                                .repeatForever())
-                        .build();
-
-                scheduler.scheduleJob(job, trigger);
             } catch (SchedulerException e) {
                 log.error("", e);
+            }
+        }
+    }
+
+    /**
+     * 启动栏目更新检查任务
+     *
+     * @param scheduler
+     */
+    private void initInfoUpdateCheckJob(Scheduler scheduler) {
+
+        // 查询数据库里面的所有站点
+        final List<MonitorSite> allMonitorSites = monitorSiteService.getAllMonitorSites();
+        if (allMonitorSites == null || allMonitorSites.isEmpty()) {
+            return;
+        }
+
+        // 每一个站点一个job
+        for (MonitorSite site : allMonitorSites) {
+            JobDetail job = newJob(CheckJob.class)
+                    .withIdentity("infoUpdateCheckJob" + String.valueOf(site.getSiteId()), "group-check")
+                    .build();
+
+            // 真正的执行任务
+            InfoUpdateCheckScheduler checkScheduler = applicationContext.getBean
+                    (InfoUpdateCheckScheduler.class);
+            checkScheduler.setSiteId(site.getSiteId());
+            checkScheduler.setBaseUrl(site.getIndexUrl());
+            job.getJobDataMap().put("task", checkScheduler);
+
+            Trigger trigger = newTrigger()
+                    .withIdentity("infoUpdateCheckJobTrigger" + String.valueOf(site.getSiteId()), "group-check")
+                    .startNow()
+                    .withSchedule(simpleSchedule()
+                            .withIntervalInSeconds(24 * 60 * 60)
+                            .repeatForever())
+                    .build();
+
+            try {
+                scheduler.scheduleJob(job, trigger);
+            } catch (SchedulerException e) {
+                log.error("failed to schedule info update check of site " + String.valueOf(site.getSiteId()), e);
             }
         }
     }
