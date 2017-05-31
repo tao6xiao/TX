ff14ad425d7578f2aa4e7c2964d80d151b546a68
helang <HL9449486@163.com>
2017-05-31 11:05:10 +0800

    删除issue表的枚举类，更改为常量来指向issue的表字段名

---
+++ src/main/java/com/trs/gov/kpi/constant/IssueTableField.java
@@ -0,0 +1,20 @@
+package com.trs.gov.kpi.constant;
+
+/**
+ * Issue表字段名常量
+ * Created by he.lang on 2017/5/31.
+ */
+public class IssueTableField {
+
+    public static final String ID = "id";
+    public static final String SITE_ID = "siteId";
+    public static final String TYPE_ID = "typeId";
+    public static final String SUBTYPE_ID = "subTypeId";
+    public static final String DETAIL = "detail";
+    public static final String ISSUE_TIME = "issueTime";
+    public static final String IS_RESOLVED = "isResolved";
+    public static final String IS_DEL = "isDel";
+    public static final String CUSTOMER1 = "customer1";
+    public static final String CUSTOMER2 = "customer2";
+    public static final String CUSTOMER3 = "customer3";
+}
