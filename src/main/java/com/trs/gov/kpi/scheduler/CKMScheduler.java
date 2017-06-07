2fd9189b982f4ea859c48f7d94807951f767f815
Wei Lin <anzhihun@gmail.com>
2017-06-07 19:23:55 +0800

    重构代码， 降低圈复杂度

--- src/main/java/com/trs/gov/kpi/scheduler/CKMScheduler.java
+++ src/main/java/com/trs/gov/kpi/scheduler/CKMScheduler.java
@@ -10,6 +10,7 @@ import com.trs.gov.kpi.service.outer.ContentCheckApiService;
 import com.trs.gov.kpi.service.outer.DocumentApiService;
 import com.trs.gov.kpi.utils.CollectionUtil;
 import com.trs.gov.kpi.utils.DBUtil;
+import com.trs.gov.kpi.utils.StringUtil;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -48,74 +49,84 @@ public class CKMScheduler implements SchedulerTask {
     @Override
     public void run() {
         log.info("CKMScheduler " + siteId + " start...");
+        List<String> checkTypeList = Types.InfoErrorIssueType.getAllCheckTypes();
         try {
-            List<Issue> issueList = new ArrayList<>();
             List<Document> documentList = documentApiService.getPublishDocuments(getSiteId());
-            List<String> checkTypeList = Types.InfoErrorIssueType.getAllCheckTypes();
-            if (checkTypeList == null || checkTypeList.isEmpty()) {
-                return;
-            }
-
-            String checkTypes = CollectionUtil.join(checkTypeList, ";");
             for (Document document : documentList) {
-
-                StringBuilder checkContent = new StringBuilder();
-                if (document.getDocTitle() != null &&  !document.getDocTitle().trim().isEmpty()) {
-                    checkContent.append(document.getDocTitle());
-                    checkContent.append("。");
-                }
-
-                if (document.getDocContent() != null && !document.getDocContent().trim().isEmpty()) {
-                    checkContent.append(document.getDocContent());
-                }
-
-                if (checkContent.length() <= 0) {
-                    continue;
-                }
-
-                ContentCheckResult result = null;
-                try {
-                    result = contentCheckApiService.check(checkContent.toString(), checkTypes);
-                } catch (Exception e) {
-                    log.error("failed to check document " + document.getChannelId() + "->" + document.getMetaDataId(), e);
-                    continue;
-                }
-
-                if (!result.isOk()) {
-                    log.error("return error: " + result.getMessage() + ", document: " + document.getChannelId() + "->" + document.getMetaDataId());
-                    continue;
-                }
-
-                if (result.getResult() != null) {
-                    for (String checkType : checkTypeList) {
-                        Types.InfoErrorIssueType subIssueType = Types.InfoErrorIssueType.valueOfCheckType(checkType);
-                        String errorContent = result.getResultOfType(subIssueType);
-                        if (errorContent == null) {
-                            continue;
-                        }
-                        Issue issue = new Issue();
-                        issue.setSiteId(document.getSiteId());
-                        issue.setTypeId(Types.IssueType.INFO_ERROR_ISSUE.value);
-                        issue.setSubTypeId(subIssueType.value);
-                        issue.setDetail(document.getDocPubUrl());
-                        issue.setIssueTime(new Date());
-                        issue.setCustomer1(errorContent);
-                        issueList.add(issue);
-                    }
-                }
+                insert(buildList(document, checkTypeList));
             }
-            //插入监测出的信息错误数据
-            for (Issue issue : issueList) {
-                issueMapper.insert(DBUtil.toRow(issue));
-            }
-            log.info("add content error count: " + issueList.size());
-        } catch (RemoteException e) {
-            log.error("", e);
-        } catch (ParseException e) {
+        } catch (RemoteException | ParseException e) {
             log.error("", e);
         } finally {
             log.info("CKMScheduler " + siteId + " end...");
         }
     }
 
+    private List<Issue> buildList(Document document, List<String> checkTypeList) {
+        List<Issue> issueList = new ArrayList<>();
+
+        StringBuilder checkContent = buildCheckContent(document);
+        if (checkContent.length() <= 0) {
+            return issueList;
+        }
+
+        ContentCheckResult result = null;
+        try {
+            result = contentCheckApiService.check(checkContent.toString(), CollectionUtil.join(checkTypeList, ";"));
+        } catch (Exception e) {
+            log.error("failed to check document " + document.getChannelId() + "->" + document.getMetaDataId(), e);
+            return issueList;
+        }
+
+        if (!result.isOk()) {
+            log.error("return error: " + result.getMessage() + ", document: " + document.getChannelId() + "->" + document.getMetaDataId());
+            return issueList;
+        }
+
+        if (result.getResult() != null) {
+            issueList = toIssueList(document, checkTypeList,result);
+        }
+        return issueList;
+    }
+
+    private List<Issue> toIssueList(Document document, List<String> checkTypeList, ContentCheckResult result) {
+        List<Issue> issueList = new ArrayList<>();
+        for (String checkType : checkTypeList) {
+            Types.InfoErrorIssueType subIssueType = Types.InfoErrorIssueType.valueOfCheckType(checkType);
+            String errorContent = result.getResultOfType(subIssueType);
+            if (StringUtil.isEmpty(errorContent)) {
+                continue;
+            }
+            Issue issue = new Issue();
+            issue.setSiteId(document.getSiteId());
+            issue.setTypeId(Types.IssueType.INFO_ERROR_ISSUE.value);
+            issue.setSubTypeId(subIssueType.value);
+            issue.setDetail(document.getDocPubUrl());
+            issue.setIssueTime(new Date());
+            issue.setCustomer1(errorContent);
+            issueList.add(issue);
+        }
+        return issueList;
+    }
+
+    private void insert(List<Issue> issueList){
+        //插入监测出的信息错误数据
+        for (Issue issue : issueList) {
+            issueMapper.insert(DBUtil.toRow(issue));
+        }
+        log.info("buildCheckContent insert error count: " + issueList.size());
+    }
+
+    private StringBuilder buildCheckContent(Document document){
+        StringBuilder checkContent = new StringBuilder();
+        if (document.getDocTitle() != null &&  !document.getDocTitle().trim().isEmpty()) {
+            checkContent.append(document.getDocTitle());
+            checkContent.append("。");
+        }
+
+        if (document.getDocContent() != null && !document.getDocContent().trim().isEmpty()) {
+            checkContent.append(document.getDocContent());
+        }
+        return checkContent;
+    }
 }
