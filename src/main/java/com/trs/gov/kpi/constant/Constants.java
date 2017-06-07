de93e96812ef7978f95b447b602eb6822b913af6
helang <HL9449486@163.com>
2017-06-07 16:57:41 +0800

    添加预警和问题的开始结束界限常量，完成问题统计中分类统计功能

--- src/main/java/com/trs/gov/kpi/constant/Constants.java
+++ src/main/java/com/trs/gov/kpi/constant/Constants.java
@@ -9,5 +9,11 @@ public final class Constants {
 
     public static final String INVALID_PARAMETER = "参数不合法！";
 
+    public static final int ISSUE_BEGIN_ID = 1;
+
+    public static final int ISSUE_END_ID = 50;
+
     public static final int WANRNING_BEGIN_ID = 51;
+
+    public static final int WANRNING_END_ID = 100;
 }
--- src/main/java/com/trs/gov/kpi/service/impl/IssueCountServiceImpl.java
+++ src/main/java/com/trs/gov/kpi/service/impl/IssueCountServiceImpl.java
@@ -1,11 +1,15 @@
 package com.trs.gov.kpi.service.impl;
 
+import com.trs.gov.kpi.constant.*;
 import com.trs.gov.kpi.dao.IssueMapper;
+import com.trs.gov.kpi.entity.dao.QueryFilter;
+import com.trs.gov.kpi.entity.dao.Table;
 import com.trs.gov.kpi.entity.responsedata.Statistics;
 import com.trs.gov.kpi.service.IssueCountService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +22,61 @@ public class IssueCountServiceImpl implements IssueCountService {
 
     @Override
     public List<Statistics> countSort(Integer[] siteIds) {
-        return null;
+        List<Statistics> list = new ArrayList<>();
+        //待解决预警
+        int waringCount = 0;
+        for (int i = 0; i < siteIds.length; i++) {
+            QueryFilter filter = new QueryFilter(Table.ISSUE);
+            filter.addCond(IssueTableField.SITE_ID, siteIds[i]);
+            filter.addCond(IssueTableField.IS_DEL, Status.Delete.UN_DELETE.value);
+            filter.addCond(IssueTableField.IS_RESOLVED, Status.Resolve.UN_RESOLVED.value);
+            filter.addCond(IssueTableField.TYPE_ID, Constants.WANRNING_BEGIN_ID).setRangeBegin(true);
+            filter.addCond(IssueTableField.TYPE_ID, Constants.WANRNING_END_ID).setRangeEnd(true);
+            int count = issueMapper.count(filter);
+            waringCount = waringCount + count;
+        }
+        Statistics statistics = getByTypeAndCount(IssueIndicator.WARNING, waringCount);
+        list.add(statistics);
+
+        //待解决问题
+        int issueCount = 0;
+        for (int i = 0; i < siteIds.length; i++) {
+            QueryFilter filter = new QueryFilter(Table.ISSUE);
+            filter.addCond(IssueTableField.SITE_ID, siteIds[i]);
+            filter.addCond(IssueTableField.IS_DEL, Status.Delete.UN_DELETE.value);
+            filter.addCond(IssueTableField.IS_RESOLVED, Status.Resolve.UN_RESOLVED.value);
+            filter.addCond(IssueTableField.TYPE_ID, Constants.ISSUE_BEGIN_ID).setRangeBegin(true);
+            filter.addCond(IssueTableField.TYPE_ID, Constants.ISSUE_END_ID).setRangeEnd(true);
+            int count = issueMapper.count(filter);
+            issueCount = issueCount + count;
+        }
+        statistics = getByTypeAndCount(IssueIndicator.UN_SOLVED_ISSUE, issueCount);
+        list.add(statistics);
+
+        //待解决问题和预警
+        statistics = getByTypeAndCount(IssueIndicator.UN_SOLVED_ALL, issueCount+waringCount);
+        list.add(statistics);
+
+        //已解决问题和预警
+        int resolvedCount = 0;
+        for (int i = 0; i < siteIds.length; i++) {
+            QueryFilter filter = new QueryFilter(Table.ISSUE);
+            filter.addCond(IssueTableField.SITE_ID, siteIds[i]);
+            filter.addCond(IssueTableField.IS_DEL, Status.Delete.UN_DELETE.value);
+            filter.addCond(IssueTableField.IS_RESOLVED, Status.Resolve.RESOLVED.value);
+            int count = issueMapper.count(filter);
+            resolvedCount = resolvedCount + count;
+        }
+        statistics = getByTypeAndCount(IssueIndicator.SOLVED_ALL, resolvedCount);
+        list.add(statistics);
+        return list;
+    }
+
+    private Statistics getByTypeAndCount(IssueIndicator type, int count) {
+        Statistics statistics = new Statistics();
+        statistics.setType(type.value);
+        statistics.setName(type.getName());
+        statistics.setCount(count);
+        return statistics;
     }
 }
