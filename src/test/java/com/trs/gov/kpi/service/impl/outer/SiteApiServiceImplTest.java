badbbe5bef410a918cff96edfa55b3f05c2c3297
linwei <lin.wei@trs.com.cn>
2017-06-07 19:56:39 +0800

    修复单元测试

--- src/test/java/com/trs/gov/kpi/service/impl/outer/SiteApiServiceImplTest.java
+++ src/test/java/com/trs/gov/kpi/service/impl/outer/SiteApiServiceImplTest.java
@@ -1,43 +1,37 @@
 package com.trs.gov.kpi.service.impl.outer;
 
 import com.squareup.okhttp.Request;
-import com.trs.gov.kpi.service.outer.OuterApiService;
-import com.trs.gov.kpi.utils.OuterApiServiceUtil;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Value;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static com.trs.gov.kpi.utils.OuterApiServiceUtil.newServiceRequestBuilder;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by linwei on 2017/5/18.
  */
 public class SiteApiServiceImplTest {
 
-    @Value("${service.outer.editcenter.url}")
-    private String editCenterServiceUrl;
-
     @Test
     public void getRequestUrl() throws Exception {
 
-        SiteApiServiceImpl apiService = new SiteApiServiceImpl();
         Map<String, String> params = new HashMap<>();
         params.put("param1", "1");
         params.put("param2", "2");
 
         // TODO fix test
-//        Request req = new OuterApiServiceUtil.ServiceRequestBuilder()
-//                .setParams(params)
-//                .setMethodName("test_func")
-//                .setServiceName("gov_site")
-//                .setUrlFormat("%s/gov/opendata.do?serviceId=%s&methodname=%s&CurrUserName=%s")
-//                .setUserName("").build();
-//
-//        String expectedUrl = editCenterServiceUrl + "/gov/opendata.do?serviceId=gov_site&methodname=test_func&CurrUserName=admin&param1=1&param2=2";
-//        assertEquals(expectedUrl, req.urlString());
+        String editCenterServiceUrl = "http://www.baidu.com";
+        Request req = newServiceRequestBuilder()
+                .setUrlFormat("%s/gov/opendata.do?serviceId=%s&methodname=%s&CurrUserName=%s")
+                .setServiceUrl(editCenterServiceUrl)
+                .setServiceName("testService")
+                .setMethodName("testMethod")
+                .setUserName("testUser")
+                .setParams(params).build();
+        String expectedUrl = editCenterServiceUrl + "/gov/opendata.do?serviceId=testService&methodname=testMethod&CurrUserName=testUser&param1=1&param2=2";
+        assertEquals(expectedUrl, req.urlString());
     }
 
 }
