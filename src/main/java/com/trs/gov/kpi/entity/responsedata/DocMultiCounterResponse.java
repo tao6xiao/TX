7aabcabe30e86f40e04807e4dcb457e9af6c2520
linwei <lin.wei@trs.com.cn>
2017-06-16 18:02:30 +0800

    添加默认值

--- src/main/java/com/trs/gov/kpi/entity/responsedata/DocMultiCounterResponse.java
+++ src/main/java/com/trs/gov/kpi/entity/responsedata/DocMultiCounterResponse.java
@@ -9,16 +9,16 @@ import java.util.Objects;
  */
 @Data
 public class DocMultiCounterResponse {
-    private Long daibianDocCount; // 待编数量,
-    private Long daishenDocCount; // 待审数量,
-    private Long daiqianDocCount; // 待签数量,
-    private Long yifaDocCount; // 已发数量,
-    private Long newDocCount; // 原稿数量
-    private Long quoteDocCount; // 链接引用数量
-    private Long mirrorDocCount; // 镜像引用数量
-    private Long copyDocCount; // 复制数量
-    private Long pushDocCount; // 上报数量
-    private Long distributeDocCount; // 下达数量
+    private Long daibianDocCount = 0L; // 待编数量,
+    private Long daishenDocCount = 0L; // 待审数量,
+    private Long daiqianDocCount = 0L; // 待签数量,
+    private Long yifaDocCount = 0L; // 已发数量,
+    private Long newDocCount = 0L; // 原稿数量
+    private Long quoteDocCount = 0L; // 链接引用数量
+    private Long mirrorDocCount = 0L; // 镜像引用数量
+    private Long copyDocCount = 0L; // 复制数量
+    private Long pushDocCount = 0L; // 上报数量
+    private Long distributeDocCount = 0L; // 下达数量
 
     @Override
     public boolean equals(Object o) {
