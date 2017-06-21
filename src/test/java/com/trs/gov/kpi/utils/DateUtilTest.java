b1651469c9cb05542dc3b0c00cbb8a6a3bfba957
ranwei <rw1031818672@163.com>
2017-06-21 14:03:58 +0800

    增加日期工具类的单元测试

--- src/test/java/com/trs/gov/kpi/utils/DateUtilTest.java
+++ src/test/java/com/trs/gov/kpi/utils/DateUtilTest.java
@@ -1,8 +1,10 @@
 package com.trs.gov.kpi.utils;
 
+import com.trs.gov.kpi.entity.HistoryDate;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -45,4 +47,43 @@ public class DateUtilTest {
         assertFalse(DateUtil.isValidMonth("2017-13"));
     }
 
+
+    @Test
+    public void splitDateByMonth() throws Exception {
+
+        //开始时间大于结束时间
+        List<HistoryDate> result = DateUtil.splitDateByMonth("2017-06-01 00:00:00", "2017-05-19 00:00:00");
+        assertEquals(0, result.size());
+
+        //开始时间和结束时间都处于临界点
+        result = DateUtil.splitDateByMonth("2017-05-31 00:00:00", "2017-06-01 00:00:00");
+        assertEquals(2, result.size());
+
+        //跨3个月
+        result = DateUtil.splitDateByMonth("2017-04-30 12:05:00", "2017-07-30 08:00:00");
+        assertEquals(4, result.size());
+
+        //起止时间在同一天
+        result = DateUtil.splitDateByMonth("2017-04-31 12:05:00", "2017-04-31 19:05:00");
+        assertEquals(1, result.size());
+
+        //跨年
+        result = DateUtil.splitDateByMonth("2016-11-30 12:05:00", "2017-02-28 19:05:00");
+        assertEquals(4, result.size());
+    }
+
+    @Test
+    public void formatOrParseDate() throws Exception {
+
+        String strDate = "2017-05-05 12:00:00";
+        Date date = DateUtil.toDate(strDate);
+        String result = DateUtil.toString(date);
+        assertEquals(strDate, result);
+
+        strDate = "2017-05-05";
+        date = DateUtil.toDayDate(strDate);
+        result = DateUtil.toDayString(date);
+        assertEquals(strDate, result);
+
+    }
 }
