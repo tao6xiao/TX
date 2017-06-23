1255eaaac119489691be00dee09f48d372320221
helang <HL9449486@163.com>
2017-06-23 18:09:56 +0800

    提交stream例子，验证传递消息和接收消息的可用性

---
+++ src/main/java/com/trs/gov/kpi/rabbitmq/RabbitSender.java
@@ -0,0 +1,30 @@
+package com.trs.gov.kpi.rabbitmq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by he.lang on 2017/6/22.
+ */
+@Service
+@Component
+@Slf4j
+public class RabbitSender {
+    @Autowired
+    private Barista source;
+
+    public String sendMessage(){
+        try {
+            Object o = new Object();
+            source.logOutPut().send(MessageBuilder.withPayload(o).build());
+            log.info("发送");
+        }catch (Exception e){
+
+        }
+        return null;
+    }
+
+}
