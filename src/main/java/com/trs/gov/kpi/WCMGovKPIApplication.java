c7b80a5a2838f765a4ae0965aeb97b385a79f81e
linwei <lin.wei@trs.com.cn>
2017-06-28 10:06:43 +0800

    注册服务到服务发现系统

--- src/main/java/com/trs/gov/kpi/WCMGovKPIApplication.java
+++ src/main/java/com/trs/gov/kpi/WCMGovKPIApplication.java
@@ -3,9 +3,11 @@ package com.trs.gov.kpi;
 import com.trs.gov.kpi.utils.SpringContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WCMGovKPIApplication {
 
 	public static void main(String[] args) {
