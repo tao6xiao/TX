f8d01ab1d555c96c107b9b10935ecdc219dc8efe
ranwei <rw1031818672@163.com>
2017-07-06 15:45:46 +0800

    修改发送消息内容

--- src/main/java/com/trs/gov/kpi/rabbitmq/RabbitSender.java
+++ src/main/java/com/trs/gov/kpi/rabbitmq/RabbitSender.java
@@ -22,14 +22,35 @@ public class RabbitSender {
 //    @InboundChannelAdapter(value = Constants.OUTPUT_CHANNEL, autoStartup = "false")
     public String sendMessage(){
         try {
-            String json = "{\n" +
-                    "    \"msgType\":\"editcenter_department_new_doc_byday\",\n" +
-                    "    \"data\":{\n" +
-                    "        \"User\":12,\n" +
-                    "        \"Department\":12,\n" +
-                    "        \"siteId\":11,\n" +
-                    "        \"CRDay\":\"2017-06-03\",\n" +
-                    "    }\n" +
+            String json = "{  \n" +
+                    "    \"TYPE\": 11,\n" +
+                    "    \"DATA\":{\n" +
+                    "         \n" +
+                    "        \"CRTIME\":\"2017-05-10 14:33:31\",\n" +
+                    "        \"DOCPUBTIME\":\"\",\n" +
+                    "        \"CHANNELID\":\"97\",\n" +
+                    "        \"DOCSTATUS\":\"10\",\n" +
+                    "        \"DOCAUTHOR\":\"聂明磊\",\n" +
+                    "        \"DOCRELTIME\":\"\",\n" +
+                    "        \"DOCSOURCENAME\":\"教育部\",\n" +
+                    "        \"DOCID\":\"1738\",\n" +
+                    "        \"DOCTITLE\":\"关于“特岗教师”解除“特岗”合同的咨询\",\n" +
+                    "        \"DOCPUBURL\":\"\",\n" +
+                    "        \"DOCLINK\":\"\",\n" +
+                    "        \"CHANNELNAME\":\"互动\",\n" +
+                    "        \"DOCTYPE\":\"20\",\n" +
+                    "        \"DOCTYPE_DISP\":\"文字\",\n" +
+                    "        \"SUBDOCTITLE\":\"\",\n" +
+                    "        \"DOCKEYWORDS\":\"\",\n" +
+                    "        \"RECID\":\"2300\",\n" +
+                    "        \"DOCCHANNEL\":\"97\",\n" +
+                    "        \"METADATAID\":\"1738\",\n" +
+                    "        \"WCMMETATABLEGOVDOCVIEWID\":\"39\",\n" +
+                    "        \"DOCABSTRACT\":\"\",\n" +
+                    "        \"CRUSER\":\"admin\",\n" +
+                    "        \"SITEID\":\"1\",\n" +
+                    "        \"CRDEPTID\":\"1\" \n" +
+                    "    }    \n" +
                     "}";
             log.info("发送");
             source.logOutPut().send(MessageBuilder.withPayload(json).build());
