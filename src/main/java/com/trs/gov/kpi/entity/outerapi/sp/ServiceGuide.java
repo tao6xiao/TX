5e3ec26785af0b9e37e4cdde28c3a316d1f460a3
ranwei <rw1031818672@163.com>
2017-07-12 13:11:09 +0800

    修改服务实用对应实体类的属性

--- src/main/java/com/trs/gov/kpi/entity/outerapi/sp/ServiceGuide.java
+++ src/main/java/com/trs/gov/kpi/entity/outerapi/sp/ServiceGuide.java
@@ -2,19 +2,19 @@ package com.trs.gov.kpi.entity.outerapi.sp;
 
 import lombok.Data;
 
-import java.util.Date;
-
 /**
  * Created by ranwei on 2017/6/12.
  */
 @Data
 public class ServiceGuide {
 
-    private String code;
+    private String code; //事项简码
 
-    private String itemName;
+    private String itemName; //事项名称
 
-    private String itemLink;
+    private String qType;//问题类型
 
-    private Date mTime;
+    private String itemLink;//事项链接
+
+    private String mTime; //监测时间
 }
