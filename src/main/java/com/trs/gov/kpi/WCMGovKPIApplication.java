f64e7c67d6f5b143542c6014396ccf8a5512bac0
linwei <lin.wei@trs.com.cn>
2017-07-13 09:26:08 +0800

    先屏蔽掉MQ和注册发现

--- src/main/java/com/trs/gov/kpi/WCMGovKPIApplication.java
+++ src/main/java/com/trs/gov/kpi/WCMGovKPIApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class WCMGovKPIApplication {
 
 	public static void main(String[] args) {
