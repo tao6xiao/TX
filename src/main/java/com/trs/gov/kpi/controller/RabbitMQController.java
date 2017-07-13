5c8274d7a87867a782be65075f9b498ed5428be5
linwei <lin.wei@trs.com.cn>
2017-07-13 09:51:00 +0800

    解决编译问题

--- src/main/java/com/trs/gov/kpi/controller/RabbitMQController.java
+++ src/main/java/com/trs/gov/kpi/controller/RabbitMQController.java
@@ -1,10 +1,6 @@
 package com.trs.gov.kpi.controller;
 
-import com.trs.gov.kpi.rabbitmq.RabbitSender;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
