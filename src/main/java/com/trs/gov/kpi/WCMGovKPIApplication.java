8807de5c23d95874be00984523220cc4dc232324
linwei <lin.wei@trs.com.cn>
2017-07-14 10:26:22 +0800

    换一个环境，修改对应的配置信息， 先注释掉自动注册

--- src/main/java/com/trs/gov/kpi/WCMGovKPIApplication.java
+++ src/main/java/com/trs/gov/kpi/WCMGovKPIApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class WCMGovKPIApplication {
 
 	public static void main(String[] args) {
