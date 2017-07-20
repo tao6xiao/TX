f024790afac7fb999289a0bb19fbd2b254ea2001
linwei <lin.wei@trs.com.cn>
2017-07-20 17:12:36 +0800

    降低爬虫的线程数， 并且修改网站监测机制， 在启动的时候不检测， 在第二天凌晨的时候才开始监测。

--- src/main/java/com/trs/gov/kpi/scheduler/LinkAnalysisScheduler.java
+++ src/main/java/com/trs/gov/kpi/scheduler/LinkAnalysisScheduler.java
@@ -64,7 +64,7 @@ public class LinkAnalysisScheduler implements SchedulerTask {
         log.info("LinkAnalysisScheduler " + siteId + " start...");
         try {
 
-            List<Pair<String, String>> unavailableUrlAndParentUrls = spider.linkCheck(5, siteId, baseUrl);
+            List<Pair<String, String>> unavailableUrlAndParentUrls = spider.linkCheck(3, siteId, baseUrl);
             Date checkTime = new Date();
             for (Pair<String, String> unavailableUrlAndParentUrl : unavailableUrlAndParentUrls) {
                 LinkAvailabilityResponse linkAvailabilityResponse = new LinkAvailabilityResponse();
