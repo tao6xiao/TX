2aaa244b9a3c91bca5bb7e823607645cb00a79cd
helang <HL9449486@163.com>
2017-07-25 14:24:39 +0800

    处理sonar爆出的坏味道，添加部分日志

--- src/main/java/com/trs/gov/kpi/ids/IDSConfig.java
+++ src/main/java/com/trs/gov/kpi/ids/IDSConfig.java
@@ -10,8 +10,6 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.servlet.DispatcherType;
-
 /**
  * Created by he.lang on 2017/6/29.
  */
