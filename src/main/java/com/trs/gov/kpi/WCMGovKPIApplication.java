84f259f54424196437c563e6c5240cdb5a1ec5fe
linwei <lin.wei@trs.com.cn>
2017-07-28 14:47:05 +0800

    修复ApplicationContext获取不到的问题

--- src/main/java/com/trs/gov/kpi/WCMGovKPIApplication.java
+++ src/main/java/com/trs/gov/kpi/WCMGovKPIApplication.java
@@ -1,16 +1,13 @@
 package com.trs.gov.kpi;
 
-import com.trs.gov.kpi.utils.SpringContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
 public class WCMGovKPIApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext app = SpringApplication.run(WCMGovKPIApplication.class, args);
-		SpringContextUtil.setApplicationContext(app);
+		SpringApplication.run(WCMGovKPIApplication.class, args);
 	}
 }
--- src/main/java/com/trs/gov/kpi/utils/SpringContextUtil.java
+++ src/main/java/com/trs/gov/kpi/utils/SpringContextUtil.java
@@ -1,36 +1,36 @@
 package com.trs.gov.kpi.utils;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by linwei on 2017/6/16.
  */
-public class SpringContextUtil {
+@Component
+public class SpringContextUtil implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
 
-    private SpringContextUtil(){
+    //设置上下文
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        SpringContextUtil.applicationContext = applicationContext;
     }
-
 
     //获取上下文
     public static ApplicationContext getApplicationContext() {
         return applicationContext;
     }
 
-    //设置上下文
-    public static void setApplicationContext(ApplicationContext applicationContext) {
-        SpringContextUtil.applicationContext = applicationContext;
-    }
-
     //通过名字获取上下文中的bean
     public static Object getBean(String name){
-        return applicationContext.getBean(name);
+        return getApplicationContext().getBean(name);
     }
 
     //通过类型获取上下文中的bean
     public static Object getBean(Class requiredType){
-        return applicationContext.getBean(requiredType);
+        return getApplicationContext().getBean(requiredType);
     }
 
 }
