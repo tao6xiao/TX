fe29a93b1779b31ce8fc493468e30ee169c59231
ranwei <rw1031818672@163.com>
2017-08-03 16:37:34 +0800

    修正问政互动相关接口

--- src/main/java/com/trs/gov/kpi/controller/InteractionController.java
+++ src/main/java/com/trs/gov/kpi/controller/InteractionController.java
@@ -1,21 +1,19 @@
 package com.trs.gov.kpi.controller;
 
-import com.trs.gov.kpi.constant.OperationType;
 import com.trs.gov.kpi.entity.exception.BizException;
 import com.trs.gov.kpi.entity.exception.RemoteException;
-import com.trs.gov.kpi.entity.outerapi.nbhd.NBHDHistoryRes;
 import com.trs.gov.kpi.entity.outerapi.nbhd.NBHDPageDataResult;
 import com.trs.gov.kpi.entity.outerapi.nbhd.NBHDRequestParam;
 import com.trs.gov.kpi.entity.outerapi.nbhd.NBHDStatisticsRes;
+import com.trs.gov.kpi.entity.responsedata.HistoryStatistics;
 import com.trs.gov.kpi.service.outer.InteractionService;
-import com.trs.gov.kpi.utils.TRSLogUserUtil;
-import com.trs.mlf.simplelog.SimpleLogServer;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by ranwei on 2017/6/9.
@@ -28,7 +26,8 @@ public class InteractionController {
     private InteractionService interactionService;
 
     /**
-     * 查询问政互动的信件列表
+     * 查询问政互动的数量
+     *
      * @param param
      * @return
      * @throws RemoteException
@@ -36,25 +35,27 @@ public class InteractionController {
     @RequestMapping(value = "/issue/bytype/count", method = RequestMethod.GET)
     public NBHDStatisticsRes getGovMsgBoxesCount(@ModelAttribute NBHDRequestParam param) throws RemoteException, BizException {
         NBHDStatisticsRes res = interactionService.getGovMsgBoxesCount(param);
-        SimpleLogServer.getInstance(TRSLogUserUtil.getLogUser()).operation(OperationType.QUERY, "查询问政互动的信件列表", "").info();
+//        SimpleLogServer.getInstance(TRSLogUserUtil.getLogUser()).operation(OperationType.QUERY, "查询问政互动的数量", "").info();
         return res;
     }
 
     /**
-     * 查询问政互动的数量
+     * 查询问政互动的数量的历史记录
+     *
      * @param param
      * @return
      * @throws RemoteException
      */
     @RequestMapping(value = "/issue/all/count/history", method = RequestMethod.GET)
-    public NBHDHistoryRes getGovMsgHistoryCount(@ModelAttribute NBHDRequestParam param) throws RemoteException, BizException {
-        NBHDHistoryRes historyRes = interactionService.getGovMsgHistoryCount(param);
-        SimpleLogServer.getInstance(TRSLogUserUtil.getLogUser()).operation(OperationType.QUERY, "查询问政互动的数量", "").info();
-        return historyRes;
+    public List<HistoryStatistics> getGovMsgHistoryCount(@ModelAttribute NBHDRequestParam param) throws RemoteException, BizException {
+        List<HistoryStatistics> historyStatisticsList = interactionService.getGovMsgHistoryCount(param);
+//        SimpleLogServer.getInstance(TRSLogUserUtil.getLogUser()).operation(OperationType.QUERY, "查询问政互动的数量的历史记录", "").info();
+        return historyStatisticsList;
     }
 
     /**
-     * 查询问政互动的数量的历史记录
+     * 查询问政互动的信件列表
+     *
      * @param param
      * @return
      * @throws RemoteException
@@ -62,7 +63,7 @@ public class InteractionController {
     @RequestMapping(value = "/msg/unhandled", method = RequestMethod.GET)
     public NBHDPageDataResult getGovMsgBoxes(@ModelAttribute NBHDRequestParam param) throws RemoteException, BizException {
         NBHDPageDataResult result = interactionService.getGovMsgBoxes(param);
-        SimpleLogServer.getInstance(TRSLogUserUtil.getLogUser()).operation(OperationType.QUERY, "查询问政互动的数量的历史记录", "").info();
+//        SimpleLogServer.getInstance(TRSLogUserUtil.getLogUser()).operation(OperationType.QUERY, "查询问政互动的信件列表", "").info();
         return result;
     }
 
--- src/main/java/com/trs/gov/kpi/entity/outerapi/nbhd/NBHDMessage.java
+++ src/main/java/com/trs/gov/kpi/entity/outerapi/nbhd/NBHDMessage.java
@@ -8,10 +8,10 @@ import lombok.Data;
 @Data
 public class NBHDMessage {
     private long crTime; //咨询时间
-    private String status; //信件状态
+    private String status; //信件流转状态
     private String queryNumber; //查询编号
     private int overtimeDays; //超期天数
     private int isDeadline; //是否超期
-    private int metadataId; //主键ID
-    private String title;
+    private int metadataId; //信件id
+    private String title;//信件标题
 }
