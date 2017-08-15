c873afbee69cac6ed75b8cd587fa8262c8955a58
linwei <lin.wei@trs.com.cn>
2017-08-15 14:41:04 +0800

    问题排除， 恢复session 共享机制

--- src/main/java/com/trs/gov/kpi/config/RedisSessionConfig.java
+++ src/main/java/com/trs/gov/kpi/config/RedisSessionConfig.java
@@ -1,11 +1,14 @@
 package com.trs.gov.kpi.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
 /**
  * Created by linwei on 2017/8/8.
  */
 
-//@Configuration
+@Configuration
 //maxInactiveIntervalInSeconds session超时时间,单位秒, 默认30分钟超时
-//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class RedisSessionConfig {
 }
