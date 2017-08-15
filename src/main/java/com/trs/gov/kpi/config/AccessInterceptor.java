ccc40488788ba3cb9890f9df5e653d4d293d86ec
ranwei <ran.wei@trs.com.cn>
2017-08-15 15:54:00 +0800

    重构代码

--- src/main/java/com/trs/gov/kpi/config/AccessInterceptor.java
+++ src/main/java/com/trs/gov/kpi/config/AccessInterceptor.java
@@ -32,35 +32,35 @@ public class AccessInterceptor extends HandlerInterceptorAdapter {
 
     private Map<String, String> getUrlPathMap() {
         Map<String, String> map = new HashMap<>();
-        map.put(Authority.KPIWEB_ISSUE_HANDLE, "/gov/kpi/issue/handle");
-        map.put(Authority.KPIWEB_ISSUE_DELETE, "/gov/kpi/issue/delete");
-        map.put(Authority.KPIWEB_ISSUE_IGNORE, "/gov/kpi/issue/ignore");
-        map.put(Authority.KPIWEB_ISSUE_UPDATEDEPT, "/gov/kpi/issue/updatedept");
+        map.put("/gov/kpi/issue/handle", Authority.KPIWEB_ISSUE_HANDLE);
+        map.put("/gov/kpi/issue/delete", Authority.KPIWEB_ISSUE_DELETE);
+        map.put("/gov/kpi/issue/ignore", Authority.KPIWEB_ISSUE_IGNORE);
+        map.put("/gov/kpi/issue/updatedept", Authority.KPIWEB_ISSUE_UPDATEDEPT);
 
-        map.put(Authority.KPIWEB_WARNING_HANDLE, "/gov/kpi/alert/handle");
-        map.put(Authority.KPIWEB_WARNING_DELETE, "/gov/kpi/alert/delete");
-        map.put(Authority.KPIWEB_WARNING_IGNORE, "/gov/kpi/alert/ignore");
-        map.put(Authority.KPIWEB_WARNING_UPDATEDEPT, "/gov/kpi/alert/updatedept");
+        map.put("/gov/kpi/alert/handle", Authority.KPIWEB_WARNING_HANDLE);
+        map.put("/gov/kpi/alert/delete", Authority.KPIWEB_WARNING_DELETE);
+        map.put("/gov/kpi/alert/ignore", Authority.KPIWEB_WARNING_IGNORE);
+        map.put("/gov/kpi/alert/updatedept", Authority.KPIWEB_WARNING_UPDATEDEPT);
 
-        map.put(Authority.KPIWEB_SERVICE_HANDLE, "/gov/kpi/service/link/issue/handle");
-        map.put(Authority.KPIWEB_SERVICE_DELETE, "/gov/kpi/service/link/issue/delete");
-        map.put(Authority.KPIWEB_SERVICE_IGNORE, "/gov/kpi/service/link/issue/ignore");
-        map.put(Authority.KPIWEB_SERVICE_UPDATEDEPT, "/gov/kpi/service/link/issue/updatedept");
+        map.put("/gov/kpi/service/link/issue/handle", Authority.KPIWEB_SERVICE_HANDLE);
+        map.put("/gov/kpi/service/link/issue/delete", Authority.KPIWEB_SERVICE_DELETE);
+        map.put("/gov/kpi/service/link/issue/ignore", Authority.KPIWEB_SERVICE_IGNORE);
+        map.put("/gov/kpi/service/link/issue/updatedept", Authority.KPIWEB_SERVICE_UPDATEDEPT);
 
-        map.put(Authority.KPIWEB_AVAILABILITY_HANDLE, "/gov/kpi/available/issue/handle");
-        map.put(Authority.KPIWEB_AVAILABILITY_DELETE, "/gov/kpi/available/issue/delete");
-        map.put(Authority.KPIWEB_AVAILABILITY_IGNORE, "/gov/kpi/available/issue/ignore");
-        map.put(Authority.KPIWEB_AVAILABILITY_UPDATEDEPT, "/gov/kpi/available/issue/updatedept");
+        map.put("/gov/kpi/available/issue/handle", Authority.KPIWEB_AVAILABILITY_HANDLE);
+        map.put("/gov/kpi/available/issue/delete", Authority.KPIWEB_AVAILABILITY_DELETE);
+        map.put("/gov/kpi/available/issue/ignore", Authority.KPIWEB_AVAILABILITY_IGNORE);
+        map.put("/gov/kpi/available/issue/updatedept", Authority.KPIWEB_AVAILABILITY_UPDATEDEPT);
 
-        map.put(Authority.KPIWEB_INFOERROR_HANDLE, "/gov/kpi/content/issue/handle");
-        map.put(Authority.KPIWEB_INFOERROR_DELETE, "/gov/kpi/content/issue/delete");
-        map.put(Authority.KPIWEB_INFOERROR_IGNORE, "/gov/kpi/content/issue/ignore");
-        map.put(Authority.KPIWEB_INFOERROR_UPDATEDEPT, "/gov/kpi/content/issue/updatedept");
+        map.put("/gov/kpi/content/issue/handle", Authority.KPIWEB_INFOERROR_HANDLE);
+        map.put("/gov/kpi/content/issue/delete", Authority.KPIWEB_INFOERROR_DELETE);
+        map.put("/gov/kpi/content/issue/ignore", Authority.KPIWEB_INFOERROR_IGNORE);
+        map.put("/gov/kpi/content/issue/updatedept", Authority.KPIWEB_INFOERROR_UPDATEDEPT);
 
-        map.put(Authority.KPIWEB_INFOUPDATE_HANDLE, "/gov/kpi/channel/issue/handle");
-        map.put(Authority.KPIWEB_INFOUPDATE_DELETE, "/gov/kpi/channel/issue/delete");
-        map.put(Authority.KPIWEB_INFOUPDATE_IGNORE, "/gov/kpi/channel/issue/ignore");
-        map.put(Authority.KPIWEB_INFOUPDATE_UPDATEDEPT, "/gov/kpi/channel/issue/updatedept");
+        map.put("/gov/kpi/channel/issue/handle", Authority.KPIWEB_INFOUPDATE_HANDLE);
+        map.put("/gov/kpi/channel/issue/delete", Authority.KPIWEB_INFOUPDATE_DELETE);
+        map.put("/gov/kpi/channel/issue/ignore", Authority.KPIWEB_INFOUPDATE_IGNORE);
+        map.put("/gov/kpi/channel/issue/updatedept", Authority.KPIWEB_INFOUPDATE_UPDATEDEPT);
         return map;
     }
 
@@ -105,87 +105,11 @@ public class AccessInterceptor extends HandlerInterceptorAdapter {
     private void checkAuthority(HttpServletRequest request) throws RemoteException, BizException {
         if (request.getRequestURI().endsWith("/handle") || request.getRequestURI().endsWith("/delete") || request.getRequestURI().endsWith("/ignore") ||
                 request.getRequestURI().endsWith("/updatedept")) {
-
             Integer siteId = paramCheckAndParse(request);
-            checkIssueRight(request, siteId);
-            checkWarningRight(request, siteId);
-            checkAvailabilityRight(request, siteId);
-            checkInfoerrorRight(request, siteId);
-            checkInfoupdateRight(request, siteId);
-            checkServiceRight(request, siteId);
-        }
-    }
-
-
-    private void checkIssueRight(HttpServletRequest request, Integer siteId) throws RemoteException, BizException {
-        if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_ISSUE_HANDLE)) >= 0) {//待解决问题
-            authorityService.checkRight(Authority.KPIWEB_ISSUE_HANDLE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_ISSUE_IGNORE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_ISSUE_IGNORE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_ISSUE_DELETE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_ISSUE_DELETE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_ISSUE_UPDATEDEPT)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_ISSUE_UPDATEDEPT, siteId);
-        }
-    }
-
-    private void checkWarningRight(HttpServletRequest request, Integer siteId) throws RemoteException, BizException {
-        if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_WARNING_HANDLE)) >= 0) {//待解决预警
-            authorityService.checkRight(Authority.KPIWEB_WARNING_HANDLE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_WARNING_IGNORE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_WARNING_IGNORE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_WARNING_DELETE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_WARNING_DELETE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_WARNING_UPDATEDEPT)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_WARNING_UPDATEDEPT, siteId);
-        }
-    }
-
-    private void checkAvailabilityRight(HttpServletRequest request, Integer siteId) throws RemoteException, BizException {
-        if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_AVAILABILITY_HANDLE)) >= 0) {//链接可用性
-            authorityService.checkRight(Authority.KPIWEB_AVAILABILITY_HANDLE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_AVAILABILITY_IGNORE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_AVAILABILITY_IGNORE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_AVAILABILITY_DELETE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_AVAILABILITY_DELETE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_AVAILABILITY_UPDATEDEPT)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_AVAILABILITY_UPDATEDEPT, siteId);
-        }
-    }
-
-    private void checkInfoerrorRight(HttpServletRequest request, Integer siteId) throws RemoteException, BizException {
-        if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_INFOERROR_HANDLE)) >= 0) {//信息错误
-            authorityService.checkRight(Authority.KPIWEB_INFOERROR_HANDLE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_INFOERROR_IGNORE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_INFOERROR_IGNORE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_INFOERROR_DELETE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_INFOERROR_DELETE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_INFOERROR_UPDATEDEPT)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_INFOERROR_UPDATEDEPT, siteId);
-        }
-    }
-
-    private void checkInfoupdateRight(HttpServletRequest request, Integer siteId) throws RemoteException, BizException {
-        if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_INFOUPDATE_HANDLE)) >= 0) {//信息更新
-            authorityService.checkRight(Authority.KPIWEB_INFOUPDATE_HANDLE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_INFOUPDATE_IGNORE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_INFOUPDATE_IGNORE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_INFOUPDATE_DELETE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_INFOUPDATE_DELETE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_INFOUPDATE_UPDATEDEPT)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_INFOUPDATE_UPDATEDEPT, siteId);
-        }
-    }
-
-    private void checkServiceRight(HttpServletRequest request, Integer siteId) throws RemoteException, BizException {
-        if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_SERVICE_HANDLE)) >= 0) {//服务实用
-            authorityService.checkRight(Authority.KPIWEB_SERVICE_HANDLE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_SERVICE_IGNORE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_SERVICE_IGNORE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_SERVICE_DELETE)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_SERVICE_DELETE, siteId);
-        } else if (request.getRequestURL().indexOf(urlPathMap.get(Authority.KPIWEB_SERVICE_UPDATEDEPT)) >= 0) {
-            authorityService.checkRight(Authority.KPIWEB_SERVICE_UPDATEDEPT, siteId);
+            String oprKey = urlPathMap.get(request.getRequestURI());
+            if (oprKey != null) {
+                authorityService.checkRight(oprKey, siteId);
+            }
         }
     }
 
