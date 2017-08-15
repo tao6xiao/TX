cc7c449144ce015dd91e3ea5cf44ddf82fbbfa0b
ranwei <ran.wei@trs.com.cn>
2017-08-15 20:05:50 +0800

    暂停session 共享

--- src/main/java/com/trs/gov/kpi/config/RedisSessionConfig.java
+++ src/main/java/com/trs/gov/kpi/config/RedisSessionConfig.java
@@ -7,8 +7,8 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  * Created by linwei on 2017/8/8.
  */
 
-@Configuration
+//@Configuration
 //maxInactiveIntervalInSeconds session超时时间,单位秒, 默认30分钟超时
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class RedisSessionConfig {
 }
