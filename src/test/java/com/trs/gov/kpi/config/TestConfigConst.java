06b172649396039382d8653c078eb4e50859e711
linwei <lin.wei@trs.com.cn>
2017-08-17 17:34:57 +0800

    新增issuemapper的单元测试， 并删除原来的ids相关jar

---
+++ src/test/java/com/trs/gov/kpi/config/TestConfigConst.java
@@ -0,0 +1,17 @@
+package com.trs.gov.kpi.config;
+
+/**
+ * 测试使用的配置常量
+ *
+ * Created by linwei on 2017/8/17.
+ */
+public class TestConfigConst {
+
+    public static final String TEST_DB_URL_PROP = "spring.datasource.url=jdbc:mysql://59.110.25.145:3306/kpidb?useUnicode=true&characterEncoding=UTF-8&useSSL=false&allowMultiQueries=true";
+    public static final String TEST_DB_USER_NAME_PROP = "spring.datasource.username=admin";
+    public static final String TEST_DB_PWD_PROP = "spring.datasource.password=trs@mlf.028";
+
+    private TestConfigConst(){}
+
+
+}
--- src/test/java/com/trs/gov/kpi/dao/IssueMapperTest.java
+++ src/test/java/com/trs/gov/kpi/dao/IssueMapperTest.java
@@ -1,78 +1,88 @@
 package com.trs.gov.kpi.dao;
 
+import com.trs.gov.kpi.config.TestConfigConst;
+import com.trs.gov.kpi.constant.IssueTableField;
 import com.trs.gov.kpi.constant.Types;
 import com.trs.gov.kpi.entity.InfoUpdate;
 import com.trs.gov.kpi.entity.Issue;
-import com.trs.gov.kpi.entity.dao.DBPager;
-import com.trs.gov.kpi.entity.dao.OrCondDBFields;
 import com.trs.gov.kpi.entity.dao.QueryFilter;
 import com.trs.gov.kpi.entity.dao.Table;
+import com.trs.gov.kpi.utils.DBUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Arrays;
+import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by linwei on 2017/5/25.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringBootTest
+
+@RunWith(SpringRunner.class)
+@MybatisTest
+@TestPropertySource(properties = {TestConfigConst.TEST_DB_URL_PROP, TestConfigConst.TEST_DB_USER_NAME_PROP, TestConfigConst.TEST_DB_PWD_PROP})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class IssueMapperTest {
 
-//    @Resource
-//    private IssueMapper issueMapper;
+    @Resource
+    private IssueMapper issueMapper;
+
+
+    private int testSiteId = 11111111;
 
     @Test
-    public void select() throws Exception {
-        QueryFilter filter = new QueryFilter(Table.ISSUE);
-
-        // 添加siteId和错误类型查询条件
-        filter.addCond("siteId", 11);
-
-        // 设置一个复合型查询条件
-        OrCondDBFields orFields = new OrCondDBFields();
-        orFields.addCond("typeId", Arrays.asList(1,2,3));
-        orFields.addCond("detail", "test").setLike(true);
-        filter.addOrConds(orFields);
-
-        // 添加一个 groupby 字段
-        filter.addGroupField("typeId");
-
-        // 添加一个升序的排序字段
-        filter.addSortField("id");
-
-        // 添加分页
-
-        filter.setPager(new DBPager(0, 10));
-
-        // 查询
-//        issueMapper.select(filter);
-    }
-
-    @Test
+    @Rollback
     public void insert() throws Exception {
+        String url = "http://www.baidu.com";
         Issue issue = new Issue();
         issue.setTypeId(Types.IssueType.INFO_UPDATE_ISSUE.value);
         issue.setSubTypeId(Types.InfoUpdateIssueType.UPDATE_NOT_INTIME.value);
-        issue.setSiteId(1);
-        issue.setDetail("http://www.baidu.com");
+        issue.setSiteId(testSiteId);
+        issue.setDetail(url);
         issue.setIssueTime(new Date());
         issue.setCustomer2("72");
-//        issueMapper.insert(DBUtil.toRow(issue));
+        issueMapper.insert(DBUtil.toRow(issue));
+
+        QueryFilter filter = new QueryFilter(Table.ISSUE);
+        filter.addCond(IssueTableField.TYPE_ID, Types.IssueType.INFO_UPDATE_ISSUE.value);
+        filter.addCond(IssueTableField.SUBTYPE_ID, Types.InfoUpdateIssueType.UPDATE_NOT_INTIME.value);
+        filter.addCond(IssueTableField.SITE_ID, testSiteId);
+        final List<Issue> issues = issueMapper.select(filter);
+        assertEquals(1, issues.size());
+        assertEquals(url, issues.get(0).getDetail());
+        assertEquals("72", issues.get(0).getCustomer2());
     }
 
     @Test
+    @Rollback
     public void insertInfoUpdate() throws Exception {
+        int chnlId = 1111;
+        String url = "http://www.baidu.com";
         InfoUpdate update = new InfoUpdate();
         update.setTypeId(Types.IssueType.INFO_UPDATE_ISSUE.value);
         update.setSubTypeId(Types.InfoUpdateIssueType.UPDATE_NOT_INTIME.value);
         update.setIssueTime(new Date());
-        update.setChnlUrl("http://www.baidu.com");
-        update.setChnlId(111);
-        update.setSiteId(1);
-//        issueMapper.insert(DBUtil.toRow(update));
+        update.setChnlUrl(url);
+        update.setChnlId(chnlId);
+        update.setSiteId(testSiteId);
+        issueMapper.insert(DBUtil.toRow(update));
+
+        QueryFilter filter = new QueryFilter(Table.ISSUE);
+        filter.addCond(IssueTableField.TYPE_ID, Types.IssueType.INFO_UPDATE_ISSUE.value);
+        filter.addCond(IssueTableField.SUBTYPE_ID, Types.InfoUpdateIssueType.UPDATE_NOT_INTIME.value);
+        filter.addCond(IssueTableField.SITE_ID, testSiteId);
+        final List<Issue> issues = issueMapper.select(filter);
+        assertEquals(1, issues.size());
+        assertEquals(String.valueOf(chnlId), issues.get(0).getCustomer2());
+        assertEquals(url, issues.get(0).getDetail());
     }
 
 }
