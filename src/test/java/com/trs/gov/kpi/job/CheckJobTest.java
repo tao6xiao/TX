541ac6c625de0a19ffa87c7dee9a71f95abffc58
helang <HL9449486@163.com>
2017-08-23 14:33:04 +0800

    BugFix：处理CheckJobTest单元测试报错的问题

--- src/test/java/com/trs/gov/kpi/job/CheckJobTest.java
+++ src/test/java/com/trs/gov/kpi/job/CheckJobTest.java
@@ -3,6 +3,8 @@ package com.trs.gov.kpi.job;
 import com.trs.gov.kpi.config.TestConfigConst;
 import com.trs.gov.kpi.scheduler.HomePageCheckScheduler;
 import com.trs.gov.kpi.scheduler.SchedulerTask;
+import com.trs.gov.kpi.service.impl.MonitorRecordServiceImpl;
+import com.trs.gov.kpi.service.impl.outer.SiteApiServiceImpl;
 import com.trs.gov.kpi.utils.ReflectionUtils;
 import com.trs.gov.kpi.utils.SpringContextUtil;
 import org.junit.Before;
@@ -24,7 +26,7 @@ import java.util.Date;
  * Created by linwei on 2017/8/18.
  */
 @RunWith(SpringRunner.class)
-@MybatisTest(includeFilters = {@ComponentScan.Filter(type = FilterType.REGEX, pattern="com.trs.*.MonitorRecordServiceImpl")})
+@MybatisTest(includeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = {MonitorRecordServiceImpl.class, SiteApiServiceImpl.class})})
 @TestPropertySource(properties = {TestConfigConst.TEST_DB_URL_PROP, TestConfigConst.TEST_DB_USER_NAME_PROP, TestConfigConst.TEST_DB_PWD_PROP})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CheckJobTest {
